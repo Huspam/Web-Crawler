291be8ac71a9e2b53896e2c03de20f4e4b2150b9
Thar Zaw <tharthar123135@gmail.com>
2024-02-02 21:10:32 -0800

    4/5 analytics; Changed extract_next_links;

--- crawler.py
+++ crawler.py
@@ -1,8 +1,7 @@
 import logging
 import re
 from urllib.parse import urlparse, urljoin
-from lxml import html
-import chardet
+from lxml import html, etree
 
 logger = logging.getLogger(__name__)
 
@@ -17,12 +16,18 @@ class Crawler:
         self.corpus = corpus
         self.subdomains = {}
         self.max_out_links = (None, -1)
+        self.longest_page = (None, -1)
+        self.freq_words = {}
+        with open('stopwords.txt') as f:
+            self.stopwords = set(f.read().split('\n'))
+
 
     def start_crawling(self):
         """
         This method starts the crawling process which is scraping urls from the next available link in frontier and adding
         the scraped links to the frontier
         """
+        i = 0
         while self.frontier.has_next_url():
             url = self.frontier.get_next_url()
             logger.info("Fetching URL %s ... Fetched: %s, Queue size: %s", url, self.frontier.fetched, len(self.frontier))
@@ -32,6 +37,16 @@ class Crawler:
                 if self.is_valid(next_link):
                     if self.corpus.get_file_name(next_link) is not None:
                         self.frontier.add_url(next_link)
+                    i +=1
+            
+            if i > 1000000:
+                break
+
+        with open('analytics.txt', 'w') as analytics_file:
+            analytics_file.write(str(self.subdomains) + '\n')
+            analytics_file.write(str(self.max_out_links) + '\n')
+            analytics_file.write(str(self.longest_page) + '\n')
+            analytics_file.write(str(sorted(self.freq_words.items(), key=lambda x: (x[1], x[0]), reverse=True)[:50]))
     
 
     def extract_next_links(self, url_data):
@@ -44,52 +59,95 @@ class Crawler:
 
         Suggested library: lxml
         """
+        url = url_data['url']
+        final_url = url_data['final_url']
         content = url_data['content']
-        decoded_content = self.get_decoded_content(content)
 
+        tree = None
+        try:
+            tree = html.fromstring(content)
+        except UnicodeDecodeError:
+            print("Invalid content format: ", url)
+            return []
+        except etree.ParserError:
+            print("Empty content: ", url)
+            return []
+        
         output_links = []
-        if decoded_content and 'html' in decoded_content:
-            tree = html.fromstring(url_data['content'])
-            links = tree.xpath('//a/@href')
+        
+        links = tree.xpath('//a/@href')
+        base_url = final_url if final_url else url
+        for link in links:
+            try:
+                output_links.append(urljoin(base_url, link))
+            except ValueError:     # Link does not appear to be an IPv4 or IPv6 address
+                print("Invalid output link: ", link)
 
-            base_url = url_data['final_url'] if url_data['final_url'] else url_data['url']
-            for link in links:
-                try:
-                    output_links.append(urljoin(base_url, link))
-                except ValueError:     # Link does not appear to be an IPv4 or IPv6 address
-                    print("Invalid link: ", link)
+        # update subdomains visited and num urls processed from subdomain
+        domain = urlparse(url).netloc
+        if domain not in self.subdomains:
+            self.subdomains[domain] = 1
+        else:
+            self.subdomains[domain] += 1
 
-            # update subdomains visited and num urls processed from subdomain
-            domain = urlparse(url_data['url']).netloc
-            if domain not in self.subdomains:
-                self.subdomains[domain] = 1
-            else:
-                self.subdomains[domain] += 1
+        # keep track of page with most valid outlinks
+        if len(output_links) > self.max_out_links[1]:
+            self.max_out_links = (url, len(output_links))
 
-            # keep track of page with most valid outlinks
-            if len(output_links) > self.max_out_links[1]:
-                self.max_out_links = (url_data['url'], len(output_links))
+        # keep track of longest page in terms of word count
+        words = self._tokenize(tree.text_content())
+        words_length = len(words)
+        if words_length > self.longest_page[1]:
+            self.longest_page = (url, words_length)
 
+        # keep track of freqwords
+        self._compute_word_frequencies(words)
+            
         return output_links
     
 
-    def get_decoded_content(self, content):
-        if not content:
-            return None
-        if isinstance(content, str):
-            return content
+    def _tokenize(self, content):
+        retlist = []
+        fast = slow = 0
+        while fast < len(content):
+            if not content[slow].isascii() or not content[slow].isalnum():
+                slow += 1
+                fast += 1
+            elif not content[fast].isascii() or not content[fast].isalnum():
+                retlist.append(''.join(content[slow:fast]).lower())
+                slow = fast
+            else:
+                fast += 1
         
-        encoding = self.detect_encoding(content)
-        if not encoding:
-            return None
-        
-        decoded_content = content.decode(encoding)
-        return decoded_content
+        return retlist
+
+
+    def _compute_word_frequencies(self, tokens):
+        for token in tokens:
+            if token not in self.stopwords:
+                if token in self.freq_words:
+                    self.freq_words[token] += 1
+                else:
+                    self.freq_words[token] = 1
     
 
-    def detect_encoding(self, content):
-        res = chardet.detect(content)
-        return res['encoding']
+    # def _get_decoded_content(self, content):
+    #     if not content:
+    #         return None
+    #     if isinstance(content, str):
+    #         return content
+        
+    #     encoding = self._detect_encoding(content)
+    #     # if not encoding:
+    #     #     return None
+        
+    #     decoded_content = content.decode(encoding)
+    #     return decoded_content
+    
+
+    # def _detect_encoding(self, content):
+    #     res = chardet.detect(content)
+    #     return res['encoding']
 
 
     def is_valid(self, url):
